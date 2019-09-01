4aff1f1af1cc74c87f6a1786a66d022b11374fcd
Marco De Filippis <mdefilippis@unowhy.com>
2019-09-01 21:41:07 +0200

    first commit

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../app.component';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent  {
+
+  @Input() post: Post;
+
+  onLike() {this.post.loveIts ++; }
+  onDislike() { this.post.loveIts --; }
+
+
+}
