cdb9354a78560bac36279f06a713ed57eac9f81f
Marco De Filippis <mdefilippis@unowhy.com>
2019-11-18 01:31:26 +0100

    feat add auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
+import { PostComponent } from './pages/posts/post/post.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { Routes, RouterModule} from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { NewPostComponent } from './pages/new-post/new-post.component'
 
+const appRoutes: Routes = [
+  { path: 'posts', component: PostsComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'newPost', component: NewPostComponent },
+  { path: '', component: PostsComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    PostsComponent,
+    AuthComponent,
+    NewPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Post } from '../app.component';
+import { Component, Input } from '@angular/core';
+import { Post } from '../posts.component';
 
 @Component({
   selector: 'app-post',
